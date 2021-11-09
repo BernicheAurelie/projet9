d56f471c5113d824b18142b979a6f6faa22e8ed4
Aurelie Berniche <berniche.aurelie@orange.fr>
2021-11-09 18:04:35 +0100

    followers done

--- followers/urls.py
+++ followers/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.followers_list, name='followers'),
     path('', views.add_followers, name='add_followers'),
-    # path('delete_followers/<int:followed_user_id>/', views.delete_followers, name='delete_followers'),
-
+    path('delete/<int:followed_user_id>/', views.delete_followers, name='delete_followers'),
+    # path('delete/', views.delete_followers, name='delete_followers')
 ]
--- followers/views.py
+++ followers/views.py
@@ -4,31 +4,33 @@ from django.shortcuts import render, redirect
 from followers.forms import AddFollowers
 
 from followers.models import UserFollows
-from users.models import User
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='connexion')
 def add_followers(request):
     context={}
-    if request.POST:
-        username = request.POST['username']
-        user = User.objects.get(username__exact=username)
-        if user is not None:
-            UserFollows.objects.create(user=request.user, followed_user=user)
-            messages.success(request, 'utilisateur ajouté')
-        else:
-            messages.success(request, "utilisateur n'existe pas")
+    try:
+        if request.POST:
+            username = request.POST['username']
+            user = User.objects.get(username__exact=username)
+            if user is not None:
+                UserFollows.objects.create(user=request.user, followed_user=user)
+                messages.success(request, 'Utilisateur ajouté avec succès.')
+            return redirect('add_followers')
+    except:
+        messages.success(request, "L'utilisateur n'existe pas.")
         return redirect('add_followers')
     context['following'] = UserFollows.objects.filter(user__exact=request.user)
     context['followed_by'] = UserFollows.objects.filter(followed_user__exact=request.user)
     return render(request, 'followers/followers.html', context)
 
-# @login_required(login_url='connexion')
-# def delete_followers(request, followed_user_id: int):
-#     if request.method == 'POST':
-#         followed_user = UserFollows.objects.get(id__exact=followed_user_id)
-#         followed_user.delete()
-#         return redirect('add_followers')
-#     return render(request, 'followers/followers.html')
+@login_required(login_url='connexion')
+def delete_followers(request, followed_user_id: int):
+    if request.method == 'POST':
+        followed_user = UserFollows.objects.get(id__exact=followed_user_id)
+        followed_user.delete()
+        return redirect('add_followers')
+    return render(request, 'followers/followers.html')
 
 
